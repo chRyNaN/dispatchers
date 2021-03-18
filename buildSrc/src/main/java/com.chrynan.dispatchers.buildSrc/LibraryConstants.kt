6cf1f79aff70deef8fde23788a81b40b05f86622
Chris <ckeenan@starry.com>
2021-03-18 23:01:02 +0700

    Bumped version to 0.2.0 (3)

--- buildSrc/src/main/java/com.chrynan.dispatchers.buildSrc/LibraryConstants.kt
+++ buildSrc/src/main/java/com.chrynan.dispatchers.buildSrc/LibraryConstants.kt
@@ -6,9 +6,9 @@ object LibraryConstants {
     const val group = "com.chrynan.dispatchers"
     const val owner = "chrynan"
     const val repoName = "dispatchers"
-    const val versionName = "0.1.1"
-    const val versionCode = 2
-    const val versionDescription = "Pre-release 0.1.1"
+    const val versionName = "0.2.0"
+    const val versionCode = 3
+    const val versionDescription = "Release 0.2.0 (3)"
     const val license = "Apache-2.0"
     const val vcsUrl = "https://github.com/chRyNaN/dispatchers.git"
     const val androidCompileSdkVersion = 29
