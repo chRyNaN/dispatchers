ac099168fdaeeab161e4bd82661ea38e53fd6e8b
Chris <ckeenan@starry.com>
2021-05-30 11:46:15 -0500

    Bumped version to 0.2.1 (4)

--- buildSrc/src/main/java/com.chrynan.dispatchers.buildSrc/LibraryConstants.kt
+++ buildSrc/src/main/java/com.chrynan.dispatchers.buildSrc/LibraryConstants.kt
@@ -6,9 +6,9 @@ object LibraryConstants {
     const val group = "com.chrynan.dispatchers"
     const val owner = "chrynan"
     const val repoName = "dispatchers"
-    const val versionName = "0.2.0"
-    const val versionCode = 3
-    const val versionDescription = "Release 0.2.0 (3)"
+    const val versionName = "0.2.1"
+    const val versionCode = 4
+    const val versionDescription = "Release 0.2.1 (4)"
     const val license = "Apache-2.0"
     const val vcsUrl = "https://github.com/chRyNaN/dispatchers.git"
 
