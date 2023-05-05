997d7d14e1ab4aa0c81b19644ec73dd9799c93b8
Chris Keenan <chrynan@pm.me>
2023-05-05 16:56:45 -0500

    Created Dispatchers.IO in common code

---
+++ dispatchers/src/commonMain/kotlin/com.chrynan.dispatchers/DispatchersUtils.kt
@@ -0,0 +1,15 @@
+@file:Suppress("unused")
+
+package com.chrynan.dispatchers
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+/**
+ * Retrieves the default [CoroutineDispatchers.io] [CoroutineDispatcher] value. Each platform provides their own
+ * instance of this [CoroutineDispatcher]. For Android, JVM, and Native platforms, there is an IO dispatcher that this
+ * will return, for the JS platform, there is no specific IO dispatcher, so the default dispatcher is returned.
+ */
+@Suppress("UnusedReceiverParameter")
+val Dispatchers.IO: CoroutineDispatcher
+    get() = dispatchers.io
