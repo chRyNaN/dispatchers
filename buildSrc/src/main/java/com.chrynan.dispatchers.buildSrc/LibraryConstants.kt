3d30a964d21c465a4d75e8ef8fa0f80630c8bf51
Chris <chrynan@pm.com>
2023-05-05 17:17:17 -0500

    Bumped version to 0.5.0 (9)

--- buildSrc/src/main/java/com.chrynan.dispatchers.buildSrc/LibraryConstants.kt
+++ buildSrc/src/main/java/com.chrynan.dispatchers.buildSrc/LibraryConstants.kt
@@ -5,16 +5,16 @@ object LibraryConstants {
     const val group = "com.chrynan.dispatchers"
     const val owner = "chrynan"
     const val repoName = "dispatchers"
-    const val versionName = "0.4.0"
-    const val versionCode = 8
+    const val versionName = "0.5.0"
+    const val versionCode = 9
     const val versionDescription = "Release $versionName ($versionCode)"
     const val license = "Apache-2.0"
     const val vcsUrl = "https://github.com/chRyNaN/dispatchers.git"
 
     object Android {
 
-        const val compileSdkVersion = 29
+        const val compileSdkVersion = 33
         const val minSdkVersion = 23
-        const val targetSdkVersion = 29
+        const val targetSdkVersion = 33
     }
 }
